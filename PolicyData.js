cec89483a4d22faa734c5371afba4e9e82501bcc
ephtgit <122044913+ephtgit@users.noreply.github.com>
2024-07-05 19:31:01 +0000

    POLICYDATA.JS UPDATE

--- PolicyData.js
+++ PolicyData.js
@@ -2,8 +2,7 @@ const policyData = {
     housing: [
         {
             title: "Zoning Reform",
-            description: "Cities would be encouraged to allow more types of housing in more areas. This could mean allowing apartment buildings in areas that previously only allowed single-family homes, or allowing shops and homes to be built in the same area.
-",
+            description: "Cities would be encouraged to allow more types of housing in more areas. This could mean allowing apartment buildings in areas that previously only allowed single-family homes, or allowing shops and homes to be built in the same area.",
             analogy: "Think of a city like a big Lego board. Right now, in many places, you're only allowed to use certain types of Lego pieces in certain areas. Zoning reform is like changing the rules so you can use more types of Lego pieces in more areas of the board, allowing you to build a more diverse and efficient city."
         },
         {
